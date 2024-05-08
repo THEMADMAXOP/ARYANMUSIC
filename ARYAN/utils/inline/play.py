eeff4bd47f9da74bd1cde32eda33c34c4109e8c4
𝐌𝐀𝐃𝐌𝐀𝐗 <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-05-08 08:41:23 +0530

    Update play.py

--- ARYAN/utils/inline/play.py
+++ ARYAN/utils/inline/play.py
@@ -66,11 +66,6 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/ABOUT_SASHIKANT/3",
-            )
-        ],
     ]
     return buttons
 
@@ -83,11 +78,6 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/ABOUT_SASHIKANT/3",
-            )
         ],
     ]
     return buttons
