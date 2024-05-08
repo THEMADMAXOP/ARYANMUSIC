1a146e3b8fb2574eb2e0721b9dfa61b9f789bf33
𝐌𝐀𝐃𝐌𝐀𝐗 <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-05-08 12:39:42 +0530

    Create repo.py

---
+++ ARYAN/plugins/aryan/repo.py
@@ -0,0 +1,83 @@
+from pyrogram import Client, filters
+import requests
+import random
+import os
+import re
+import asyncio
+import time
+from ARYAN import app
+from ARYAN.utils.database import add_served_chat_clone, delete_served_chat_clone
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from ARYAN.utils.database import get_assistant
+import asyncio
+from ARYAN.misc import SUDOERS
+from ARYAN.core.userbot import Userbot
+from pyrogram import Client, filters
+from pyrogram.errors import UserAlreadyParticipant
+from ARYAN import app
+import asyncio
+import random
+from pyrogram import Client, filters
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors import (
+    ChatAdminRequired,
+    InviteRequestSent,
+    UserAlreadyParticipant,
+    UserNotParticipant,
+)
+from ARYAN import app
+
+
+
+@Client.on_message(filters.command("repo") & filters.group)
+async def repo(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/4b52da6d880cbb199298a.jpg",
+        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
+                    )
+                ]
+            ]
+        ),
+    )
+
+
+@Client.on_message(filters.command("repo") & filters.group)
+async def help(client: Client, message: Message):
+
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/4b52da6d880cbb199298a.jpg",
+        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
+                    )
+                ]
+            ]
+        ),
+    )
+
+
+@Client.on_message(filters.command("repo") & filters.private)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/4b52da6d880cbb199298a.jpg",
+        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
+                    )
+                ]
+            ]
+        ),
+    )
+
+
