9e266d57a2768fd3623a5c84f7d0acac66d18b7a
𝐌𝐀𝐃𝐌𝐀𝐗 <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-05-08 08:03:44 +0530

    Update extras.py

--- ARYAN/utils/inline/extras.py
+++ ARYAN/utils/inline/extras.py
@@ -1,5 +1,9 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+from ARYAN import app
+
+import config
+
 from config import SUPPORT_CHAT
 
 
@@ -37,5 +41,102 @@ def supp_markup(_):
                 ),
             ]
         ]
+    )
+    return upl
+
+def add_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ", url=f"https://t.me/{app.username}?startgroup=True&admin=delete_messages+invite_users"),
+           ],
+        ]
+    )
+    return upl
+
+def suppclose_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                 InlineKeyboardButton(
+                    text=_["S_B_9"],
+                    url=SUPPORT_CHAT,
+            ),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"],
+                    callback_data="close",
+                ),
+            ]
+        ]
+    )
+    return upl
+
+def source_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+           [ 
+                InlineKeyboardButton(
+                        text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper"
+                    ),
+                    InlineKeyboardButton(
+                        text="ᴄʟᴏsᴇ", callback_data=f"close"
+                    ),
+           ],
+        ]
+    )
+    return upl
+
+def lood_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+                [
+                    InlineKeyboardButton(
+                        text="ᴄʜᴀɴɴᴇʟ", url="https://t.me/STATUSDAIRY2"
+                    ),
+
+                    InlineKeyboardButton(
+                        text="ᴍᴜsɪᴄ ɢʀᴏᴜᴘ", url="https://t.me/vohmusic"
+                    ),
+                    
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=config.OWNER_ID
+                    ),
+
+                    InlineKeyboardButton(
+                        text="sᴜᴘᴘᴏʀᴛ", url="https://t.me/VOICEOFHEART0"
+                    ),
+                    
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper"
+                    )
+                ],
+            ]
+    )
+    return upl
+
+def lood2_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+                [
+                    InlineKeyboardButton(
+                        text="ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=config.OWNER_ID
+                    ),
+
+                    InlineKeyboardButton(
+                        text="sᴜᴘᴘᴏʀᴛ", url="https://t.me/VOICEOFHEART0"
+                    ),
+                    
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="ᴄʟᴏsᴇ", callback_data=f"close"
+                    )
+                ],
+            ]
     )
     return upl
